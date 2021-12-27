c138a1cc8b1b7314b151bec23c2cc4d213efa86e
Christopher Bentkowski <chris.bentkowski@gmail.com>
2021-12-27 05:00:52 -0600

    Added hangman graphics

--- hangman.py
+++ hangman.py
@@ -1,19 +1,82 @@
 import json
 import random
 import turtle
+from pathlib import Path
+
+source_path = Path(__file__).resolve()
+source_dir = source_path.parent
+
+### Load list of words ###
+with open(str(source_dir) + '/words.json') as f:
+    words = json.load(f)
+
+### Global Variables ###
+word = ""
+wordlist = []
+paQuestion = 'Do you want to play again?'
+used_Letters = []
+numguesses = 8
+correctletters = []
+gameIsDone = False
 
 ### Setup Turtle ###
 t = turtle.Turtle(visible=False)
+t.hideturtle()
 t.speed(50)
 t.pensize(5)
 t.penup()
 
-### Start Writing Turtle ###
+### Start Word Writing Turtle ###
 w = turtle.Turtle(visible=False)
+w.hideturtle()
 align = "left"
 font=("Arial", 36, "bold")
 w.penup()
 w.goto(-300,-300)
+
+### Start Used Letter Writing Turtle ###
+ul = turtle.Turtle(visible=False)
+ul.hideturtle()
+ulfont=("Arial", 24, "bold")
+ul.penup()
+ul.goto(-375,300)
+
+### Function Setup Game ###
+def setupGame():
+    ### Setup Globals ###
+    global used_Letters
+    global numguesses
+    global correctletters
+    global gameIsDone
+    global word
+    global words
+    global wordlist
+
+    used_Letters = []
+    numguesses = 8
+    correctletters = []
+    t.clear()
+    t.reset()
+    t.hideturtle()
+    t.speed(50)
+    t.pensize(5)
+    w.clear()
+    w.reset()
+    w.hideturtle()
+    w.speed(50)
+    ul.clear()
+    ul.reset()
+    ul.hideturtle()
+    ul.speed(50)
+    gameIsDone = False
+        
+    ### Randomly select word from list ###
+    word = random.choice(words).lower()
+
+    ### Create list from word ###
+    wordlist = list(word)
+
+    return
 
 ### Function Enter a Letter ###
 from pip._vendor.distlib.compat import raw_input
@@ -43,9 +106,71 @@ def buildFirstStand():
     t.goto(350,350)
     t.penup()
 
+### Build Second Part of Hangman Stand ###
+def buildSecondStand():
+    t.penup()
+    t.goto(350,350)
+    t.pendown()
+    t.goto(200,350)
+    t.goto(200,300)
+    t.penup()
+
+### Build Head ###
+def buildHead():
+    t.penup()
+    t.goto(200,200)
+    t.pendown()
+    t.circle(50)
+    t.penup()
+
+### Build Body ###
+def  buildBody():
+    t.penup()
+    t.goto(200,200)
+    t.pendown()
+    t.goto(200,-50)
+    t.penup()
+
+### Build Left Arm ###
+def buildLeftArm():
+    t.penup()
+    t.goto(200,100)
+    t.pendown()
+    t.setheading(180-45)
+    t.forward(100)
+    t.penup()
+
+### Build Right Arm ###
+def buildRightArm():
+    t.penup()
+    t.goto(200,100)
+    t.pendown()
+    t.setheading(45)
+    t.forward(100)
+    t.penup()
+
+### Build Left Leg ###
+def buildLeftLeg():
+    t.penup()
+    t.goto(200,-50)
+    t.setheading(270-45)
+    t.pendown()
+    t.forward(150)
+    t.penup()
+
+### Build Right Leg ###
+def buildRightLeg():
+    t.penup()
+    t.goto(200,-50)
+    t.setheading(270+45)
+    t.pendown()
+    t.forward(150)
+    t.penup()
+
 ### Build Board ###
-def build_board(correctletters, secretWord):
-    used_letters()
+def buildWord(correctletters, secretWord):
+    usedLetters()
+    writeWord = ""
     blanks = '_' * len(secretWord)
     
     for i in range(len(secretWord)):
@@ -53,15 +178,28 @@ def build_board(correctletters, secretWord):
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
     
     for letter in blanks:
-        print(letter, end=' ')
+        writeWord += letter + " "
+
+    w.clear()
+    w.penup()
+    w.goto(-300,-300)
+    w.write(writeWord, font=font, align=align)
+    w.penup()
     
-    print()
+def usedLetters():
+    writeUsedLetters = ""
     
-def used_letters():
-    print("Used Letters: ", end='')
-    for letter in usedLetters:
-        print(letter, end=' ')
-    print()
+    if len(used_Letters) > 0:
+        for letter in used_Letters:
+            writeUsedLetters += letter + " "
+    
+    ul.clear()
+    ul.penup()
+    ul.goto(-375,300)
+    ul.write("Used Letters:", font=ulfont, align=align)
+    ul.goto(-375, 250)
+    ul.write(writeUsedLetters, font=ulfont, align=align)
+    ul.penup()
 
 def playAgain(question):
     reply = str(raw_input(question+' (y/n): ').lower().strip())
@@ -71,28 +209,41 @@ def playAgain(question):
         return False
     else:
         return playAgain(paQuestion)
-    
 
-### Load list of words ###
-with open('words.json') as f:
-    words = json.load(f)
+def buildHangman(numguesses):
+    match numguesses:
+        case 7:
+            buildFirstStand()
+            return
+        case 6:
+            buildSecondStand()
+            return
+        case 5:
+            buildHead()
+            return
+        case 4:
+            buildBody()
+            return
+        case 3:
+            buildLeftArm()
+            return
+        case 2:
+            buildRightArm()
+            return
+        case 1:
+            buildLeftLeg()
+            return
+        case 0:
+            buildRightLeg()
+            return
     
-### Randomly select word from list ###
-word = random.choice(words).lower()
-### Create list from word ###
-wordlist = list(word)
-
 ### Setup Game ###
-paQuestion = 'Do you want to play again?'
-usedLetters = []
-numguesses = 6
-correctletters = []
-gameIsDone = False
+setupGame()
 
 while True:
-    build_board(correctletters, word)
-    letterguess = enter_letter(usedLetters)
-    usedLetters.append(letterguess)
+    buildWord(correctletters, word)
+    letterguess = enter_letter(used_Letters)
+    used_Letters.append(letterguess)
     if letterguess in wordlist :
         correctletters.append(letterguess)
         ### Check for Win ###
@@ -102,10 +253,12 @@ while True:
                 foundAllLetters = False
                 break
         if foundAllLetters:
+            buildWord(correctletters, word)
             print("You've won the game! The word was " + word +".")
             gameIsDone = True
     else:
         numguesses = numguesses - 1
+        buildHangman(numguesses)
         if numguesses == 0:
             print("You have run out of guesses. The word was " + word + ".")
             gameIsDone = True
@@ -113,11 +266,6 @@ while True:
     ### End Game ###
     if gameIsDone:
         if playAgain(paQuestion):
-            usedLetters = []
-            numguesses = 6
-            correctletters = []
-            gameIsDone = False
-            word = random.choice(words).lower()
-            wordlist = list(word)
+            setupGame()
         else:
             break
